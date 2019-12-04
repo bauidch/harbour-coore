fb0c339114dd37c4f912cbe7a17a3f2bd3bec047
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2019-12-05 00:30:50 +0800

    Create harbour-zh_CN.ts

---
+++ translations/harbour-zh_CN.ts
@@ -0,0 +1,106 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <source>Coore version: 0.7</source>
+        <translation>Coore 版本：0.7</translation>
+    </message>
+    <message>
+        <source>Author:</source>
+        <translation>作者：</translation>
+    </message>
+    <message>
+        <source>bauidch</source>
+        <translation>bauidch</translation>
+    </message>
+    <message>
+        <source>Thanks:</source>
+        <translation>感谢：</translation>
+    </message>
+    <message>
+        <source>To STJEREM unofficial coop API</source>
+        <translation>STJEREM 提供非官方 coop API</translation>
+    </message>
+    <message>
+        <source>Data:</source>
+        <translation>数据：</translation>
+    </message>
+    <message>
+        <source>All Menus come from the Coop Website.</source>
+        <translation>所有菜单均来自 Coop 网站。 </translation>
+    </message>
+    <message>
+        <source>All information is supplied without guarantee.</source>
+        <translation>所有信息均在没有提供担保的情况下提供。</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Coore</source>
+        <translation>Coore</translation>
+    </message>
+</context>
+<context>
+    <name>FavoritePage</name>
+    <message>
+        <source>Remove</source>
+        <translation>移除</translation>
+    </message>
+    <message>
+        <source>Removing</source>
+        <translation>正在移除</translation>
+    </message>
+    <message>
+        <source>No Entry</source>
+        <translation>无入口</translation>
+    </message>
+</context>
+<context>
+    <name>LocationPage</name>
+    <message>
+        <source>Add to Favorites</source>
+        <translation>添加到收藏</translation>
+    </message>
+    <message>
+        <source>No Menus</source>
+        <translation>无菜单</translation>
+    </message>
+    <message>
+        <source>no connection or no menu available</source>
+        <translation>没有可用连接或菜单</translation>
+    </message>
+    <message>
+        <source>Today</source>
+        <translation>今天</translation>
+    </message>
+</context>
+<context>
+    <name>MenuPage</name>
+    <message>
+        <source>Coore Restaurant </source>
+        <translation>Coore 餐馆</translation>
+    </message>
+</context>
+<context>
+    <name>SearchRestaurantPage</name>
+    <message>
+        <source>Location</source>
+        <translation>位置</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <source>Favorites</source>
+        <translation>收藏</translation>
+    </message>
+</context>
+</TS>
